db01a3d8524ff78b1f853c6a19400b053e7f5ad0
maxym-huklivsky <106371809+maxym-huklivsky@users.noreply.github.com>
2022-11-12 15:32:10 +0200

    task 1 is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const body = document.body;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let idForInt = null;
 
+btnStart.addEventListener('click', onRunChanger);
+btnStop.addEventListener('click', onStopChanger);
+
+function onRunChanger(e) {
+  e.currentTarget.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled');
+
+  changeBodyStyle();
+
+  idForInt = setInterval(changeBodyStyle, 1000);
+}
+
+function onStopChanger(e) {
+  e.currentTarget.setAttribute('disabled', 'disabled');
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(idForInt);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyStyle() {
+  body.style.backgroundColor = getRandomHexColor();
+}
