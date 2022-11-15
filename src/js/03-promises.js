3aec9e442a53488182e427a9df06d53f5ebd7a60
maxym-huklivsky <106371809+maxym-huklivsky@users.noreply.github.com>
2022-11-15 13:47:02 +0200

    task 3 is completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', generatePromises);
+
+function generatePromises(e) {
+  e.preventDefault();
+
+  const formElms = e.currentTarget.elements;
+  const step = formElms.step.value;
+  const firstDelay = formElms.delay.value;
+  let currentDelayForTimeOut = Number(firstDelay);
+  let currentDelay = Number(firstDelay);
+
+  for (let i = 1; i <= formElms.amount.value; i++) {
+    const shouldResolve = Math.random() > 0.3;
+    const promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          res({ position: i, delay: currentDelay });
+          currentDelay += Number(step);
+        } else {
+          rej({ position: i, delay: currentDelay });
+          currentDelay += Number(step);
+        }
+      }, currentDelayForTimeOut);
+    });
+
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    currentDelayForTimeOut += Number(step);
   }
 }
